ed8c1e7df3b736f40a876b601e58c2eded611c51
Devp-sriram <sriramraman100@gmail.com>
2024-08-19 15:15:45 +0530

    more concepts added

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,22 +1,26 @@
-const { readFile,writeFile } = require('fs');
-console.log('start')
-readFile('./content/first.txt','utf8',(err,result)=>{
-    if(err){
-        console.log(err)
-    }
-    const first = result;
-    readFile('./content/second.txt','utf8',(err,result)=>{
-    if(err){
-        console.log(err)
+const { readFile, writeFile } = require("fs");
+console.log("start");
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
     }
     const second = result;
-    writeFile('./content/result-async.txt',`${first},\n${second}`,{flag : 'a'},(err,result)=>{
-        if(err){
-            return err
+    writeFile(
+      "./content/result-async.txt",
+      `${first},\n${second}`,
+      { flag: "a" },
+      (err, result) => {
+        if (err) {
+          return err;
         }
-    console.log('done with this task');
-
-    });
-    })
-})
-console.log('starting with next task');
+        console.log("done with this task");
+      }
+    );
+  });
+});
+console.log("starting with next task");
---
+++ 12-os-module.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.write('welcome to home')
+    }else if(req.url === '/about'){
+        res.write('here is the short history')
+    }else{
+        res.write(`
+            <h1>Oops!</h1>
+            <p> the page you are looking for is not exist </p>
+            <a href='/'>back to home </a>
+            `)
+    }
+    res.end()
+});
+
+
+server.listen(5000);
